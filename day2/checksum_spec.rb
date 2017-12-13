9a66638e6bc9e705d1a63b0e67ad77476117f312
Carlos Cuellar <carlos.cuellar@plxis.com>
2017-12-13 13:33:58 -0500

    Complete solution for step 1 day 2

--- day2/checksum_spec.rb
+++ day2/checksum_spec.rb
@@ -7,4 +7,17 @@ describe 'Checksum' do
     it { expect(Checksum.line_diff([1,5,5,9])).to eq 8 }
     it { expect(Checksum.line_diff([9,5,5,2])).to eq 7 }
   end
+
+  describe 'checksum calculation for a matrix' do
+    let(:m) do
+      [
+        [5, 1, 9, 5],
+        [7, 5, 3],
+        [2, 4, 6, 8],
+      ]
+    end
+
+    it { expect(Checksum.of_matrix([[]])).to eq 0 }
+    it { expect(Checksum.of_matrix(m)).to eq 18 }
+  end
 end
