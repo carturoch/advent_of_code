029f20dd224fba6f22af50afa07d97cd1923a98e
Carlos Cuellar <carturoch@gmail.com>
2018-12-01 12:17:16 -0500

    Detect duplications

--- 2018/day1/chronal_calibration.rb
+++ 2018/day1/chronal_calibration.rb
@@ -1,24 +1,49 @@
 class ChronalCalibration
   attr_accessor :result
+  attr_accessor :detected_frequencies
 
   def initialize
     self.result = 0
+    self.detected_frequencies = [0]
+    self.duplicated = nil
   end
 
-  def apply(str)
+  def apply(str, stop_on_repeated: false)
+    return self.duplicated if duplicated?
+    
     self.result += str.to_i
+    check_duplication
   end
+
+  def duplicated?
+    !self.duplicated.nil?
+  end
+
+protected
+
+  attr_accessor :duplicated
+
+  def check_duplication
+    if detected_frequencies.include? self.result
+      self.duplicated = self.result
+    else
+      self.detected_frequencies << self.result
+    end
+  end
+
 end
 
+# ## STEP 1
+# 
 # Simpler solution
 # 
 # p File
 #   .read('input.txt')
 #   .split("\n")
 #   .reduce(0) { |memo, v| memo + v.to_i }
-
+# ----------------------------------------------------
 # OO way
 # 
-# calibrator = ChronalCalibration.new
-# IO.foreach('input.txt') { |v| calibrator.apply(v) }
-# p calibrator.result 
+# device = ChronalCalibration.new
+# IO.foreach('input.txt') { |v| device.apply(v) }
+# p device.result 
--- 2018/day1/chronal_calibration_spec.rb
+++ 2018/day1/chronal_calibration_spec.rb
@@ -8,6 +8,14 @@ describe ChronalCalibration do
     it 'result is zero by default' do
       expect(subject.result).to eq 0
     end
+
+    it 'sets a hash to check for duplicated' do
+      expect(subject.detected_frequencies).to eq [0]
+    end
+
+    it 'sets duplicated flag to false by default' do
+      expect(subject).not_to be_duplicated
+    end
   end
 
   describe 'apply' do
@@ -27,6 +35,16 @@ describe ChronalCalibration do
       subject.apply('-4')
 
       expect(subject.result).to eq 2
+    end
+
+    context 'when considering duplicated frequencies' do
+      it 'sets the flag when detects duplicated' do
+        subject.apply('+1', stop_on_repeated: true)
+        subject.apply('-1', stop_on_repeated: true)
+
+        expect(subject).to be_duplicated
+        expect(subject.result).to eq 0
+      end
     end
   end
 end
