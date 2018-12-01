e836d48af5db3704422ff02b7819341e79420a32
Carlos Cuellar <carturoch@gmail.com>
2018-12-01 10:55:05 -0500

    Solve step 1

--- 2018/day1/chronal_calibration.rb
+++ 2018/day1/chronal_calibration.rb
@@ -9,3 +9,16 @@ class ChronalCalibration
     self.result += str.to_i
   end
 end
+
+# Simpler solution
+# 
+# p File
+#   .read('input.txt')
+#   .split("\n")
+#   .reduce(0) { |memo, v| memo + v.to_i }
+
+# OO way
+# 
+# calibrator = ChronalCalibration.new
+# IO.foreach('input.txt') { |v| calibrator.apply(v) }
+# p calibrator.result 
