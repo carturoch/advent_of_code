6bd4a1a1884a610478822504bb38850f79a3a3de
Carlos Cuellar <carturoch@gmail.com>
2019-12-01 21:59:08 -0500

    Add day1 step 2 solution

--- 2019/day1/fuel.go
+++ 2019/day1/fuel.go
@@ -17,12 +17,32 @@ func FuelPerModule(mass int) int {
 	return 0
 }
 
+// DeepFuelPerModule calculates the total required fuel
+// recursively for all the masses of fuel as well
+func DeepFuelPerModule(mass int) int {
+	fuel := FuelPerModule(mass)
+	if fuel > 0 {
+		return fuel + DeepFuelPerModule(fuel)
+	}
+	return fuel
+}
+
 func step1(scanner *bufio.Scanner) int {
 	total := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		mass, _ := strconv.Atoi(line)
 		total = total + FuelPerModule(mass)
+	}
+	return total
+}
+
+func step2(scanner *bufio.Scanner) int {
+	total := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		mass, _ := strconv.Atoi(line)
+		total = total + DeepFuelPerModule(mass)
 	}
 	return total
 }
@@ -36,5 +56,6 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	fmt.Println(step1(scanner))
+	// fmt.Println(step1(scanner))
+	fmt.Println(step2(scanner))
 }
--- 2019/day1/fuel_test.go
+++ 2019/day1/fuel_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFuelPerModule(t *testing.T) {
 	type args struct {
@@ -36,6 +38,35 @@ func TestFuelPerModule(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := FuelPerModule(tt.args.mass); got != tt.want {
 				t.Errorf("FuelPerModule() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDeepFuelPerModule(t *testing.T) {
+	type args struct {
+		mass int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"Small mass",
+			args{14},
+			2,
+		},
+		{
+			"Regular mass",
+			args{1969},
+			966,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DeepFuelPerModule(tt.args.mass); got != tt.want {
+				t.Errorf("DeepFuelPerModule() = %v, want %v", got, tt.want)
 			}
 		})
 	}
