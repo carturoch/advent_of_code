ec14e828aef82bc3cd990af25aa2f3672bfcc2ce
Carlos Cuellar <carturoch@gmail.com>
2019-12-07 17:35:40 -0500

    Brutforce step 2

--- 2019/day2/opcodes.go
+++ 2019/day2/opcodes.go
@@ -26,6 +26,19 @@ func readInput() []int {
 	return program
 }
 
+func outbound(ref, size int) bool {
+	return ref < 0 || ref >= size
+}
+
+func anyOutbound(references []int, upper int) bool {
+	for _, ref := range references {
+		if outbound(ref, upper) {
+			return true
+		}
+	}
+	return false
+}
+
 // Exec process each if the instructions in the program
 func Exec(instructions []int) []int {
 	size := len(instructions)
@@ -34,6 +47,11 @@ func Exec(instructions []int) []int {
 		ref1 := instructions[index+1]
 		ref2 := instructions[index+2]
 		ref3 := instructions[index+3]
+
+		if anyOutbound([]int{ref1, ref2, ref3}, size) {
+			return []int{0, 0, 0, 0}
+		}
+
 		val1 := instructions[ref1]
 		val2 := instructions[ref2]
 
@@ -49,6 +67,35 @@ func Exec(instructions []int) []int {
 	return instructions
 }
 
+func result(noun, verb int, instructions []int) int {
+	instructions[1] = noun
+	instructions[2] = verb
+	res := Exec(instructions)
+	return res[0]
+}
+
+func bf(target int, instructions []int) (int, int) {
+	max := 99
+	var res int
+	size := len(instructions)
+	for noun := 0; noun <= max; noun++ {
+		for verb := 0; verb <= max; verb++ {
+			disposableInstructions := make([]int, size)
+			copy(disposableInstructions, instructions)
+			res = result(noun, verb, disposableInstructions)
+			if res == target {
+				return noun, verb
+			}
+		}
+	}
+	return 0, 0
+}
+
 func main() {
-	fmt.Println(Exec(readInput()))
+	// input := readInput()
+	// fmt.Println(result(12, 2, input))
+
+	target := 19690720
+	noun, verb := bf(target, readInput())
+	fmt.Println(noun, verb)
 }
